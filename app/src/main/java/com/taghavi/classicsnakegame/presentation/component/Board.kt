e91f5836bf8d4057c64f7a5830e606d2c22c5948
EhsanTgv <EhsanTgv@gmail.com>
2023-08-07 15:06:09 +0300

    Implement Board

---
+++ app/src/main/java/com/taghavi/classicsnakegame/presentation/component/Board.kt
@@ -0,0 +1,49 @@
+package com.taghavi.classicsnakegame.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.taghavi.classicsnakegame.data.model.State
+import com.taghavi.classicsnakegame.domain.game.GameEngine
+import com.taghavi.classicsnakegame.ui.theme.DarkGreen
+import com.taghavi.classicsnakegame.ui.theme.border2dp
+import com.taghavi.classicsnakegame.ui.theme.corner4dp
+import com.taghavi.classicsnakegame.ui.theme.padding16dp
+
+@Composable
+fun Board(state: State) {
+    BoxWithConstraints(Modifier.padding(padding16dp)) {
+        val tileSize = maxWidth / GameEngine.BOARD_SIZE
+        Box(
+            Modifier
+                .size(maxWidth)
+                .border(border2dp, DarkGreen)
+        )
+        Box(
+            Modifier
+                .offset(x = tileSize * state.food.first, y = tileSize * state.food.second)
+                .size(tileSize)
+                .background(
+                    DarkGreen, CircleShape
+                )
+        )
+        state.snake.forEach {
+            Box(
+                modifier = Modifier
+                    .offset(x = tileSize * it.first, y = tileSize * it.second)
+                    .size(tileSize)
+                    .background(
+                        DarkGreen, RoundedCornerShape(corner4dp)
+                    )
+            )
+        }
+    }
+}
